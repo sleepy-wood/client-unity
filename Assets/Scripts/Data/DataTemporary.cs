63dbe5b385e3f3cd9168d989edc4a57698bf03fb
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-10-20 20:12:59 +0900

    update

---
+++ Assets/Scripts/Data/DataTemporary.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataTemporary
+{
+    public static UserData MyUserData = null;
+    public static LandData MyLandData = null;
+
+}
--- Assets/Scripts/Land/LandData.cs
+++ Assets/Scripts/Land/LandData.cs
@@ -2,46 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+#nullable enable
 /// <summary>
 /// 땅 위에 있는 오브젝트들의 정보를 저장하는 구조체
 /// </summary>
 [Serializable]
-public struct ObjectsInfo
+public class ObjectsInfo
 {
-    public string path;
-    public Vector3 localPosition;
-    public Vector3 localScale;
-    public Vector3 localEulerAngle;
+    public string? path = null;
+    public Vector3? localPosition = null;
+    public Vector3? localScale = null;
+    public Vector3? localEulerAngle = null;
 }
 
 /// <summary>
 /// 땅위에 있는 오브젝트들의 List
 /// </summary>
 [Serializable]
-public struct ArrayObjectsOfLand
+public class ArrayObjectsOfLand
 {
-    public List<ObjectsInfo> Objects;
+    public List<ObjectsInfo>? Objects = null;
 }
 
 /// <summary>
 /// Land 하나에 있는 정보들
 /// </summary>
 [Serializable]
-public struct LandData
+public class LandData
 {
-    public int landNum;
-    public Vector3 landPosition;
-    public Vector3 landScale;
-    public Vector3 landEulerAngle;
-    public ArrayObjectsOfLand arrayObjectsOfLand;
+    public int? landNum = null;
+    public Vector3? landPosition = null;
+    public Vector3? landScale = null;
+    public Vector3? landEulerAngle = null;
+    public ArrayObjectsOfLand? arrayObjectsOfLand = null;
 }
 
 /// <summary>
 /// 여러 Land들의 정보를 담을 List
 /// </summary>
 [Serializable]
-public struct ArrayLandData
+public class ArrayLandData
 {
-    public List<LandData> LandLists;
+    public List<LandData>? LandLists= null;
 }
