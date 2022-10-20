d24f88b3d4cad11851c40d313790e9152f2e4246
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-10-20 17:36:02 +0900

    update

--- Assets/Scripts/Other/Charactor/CharactorChoice.cs
+++ Assets/Scripts/Other/Charactor/CharactorChoice.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharactorChoice : MonoBehaviour
 {
-    public void OnClickPreviewCharactor()
+    public Transform content;
+    public void OnClickPreviewCharactor(int index)
     {
+        for (int i = 0; i < content.childCount; i++)
+        {
+            if (i == index)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+                content.GetChild(i).GetComponent<Image>().color = Color.gray;
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+                content.GetChild(i).GetComponent<Image>().color = Color.white;
+            }
+        }
 
     }
 }
