a1cba216fbe0ab29b19f7489ca7586b11a67eae7
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-10-20 13:35:28 +0900

    update Charactor

--- Assets/Scripts/Other/Charactor/CharactorChoice.cs
+++ Assets/Scripts/Other/Charactor/CharactorChoice.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class CharactorChoice : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickPreviewCharactor()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
