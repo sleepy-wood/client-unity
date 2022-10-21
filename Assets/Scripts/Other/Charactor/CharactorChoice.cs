30bef7b7c4481891cdab2824bc912ce09aa94793
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-10-21 14:04:21 +0900

    update CharactorChoice

--- Assets/Scripts/Other/Charactor/CharactorChoice.cs
+++ Assets/Scripts/Other/Charactor/CharactorChoice.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks.Triggers;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,17 +12,62 @@ public class CharactorChoice : MonoBehaviour
     public Transform content;
     public int selectedIndex;
     public Transform explainText;
+    public float textSpeed = 7f;
 
     private UserInput userInput;    
     private void Start()
     {
         OnClickPreviewCharactor(0);
         userInput = GetComponent<UserInput>();
+        for (int i = 0; i < explainText.childCount; i++)
+        {
+            StartCoroutine(AlphaText(i));
+        }
     }
     private void Update()
     {
+        //Rotate를 하면 안내문 끄기
+        if (userInput.Rotate != 0 && explainText.gameObject.activeSelf)
+        {
+            StopAllCoroutines();
+            explainText.gameObject.SetActive(false);
+        }
+
         transform.GetChild(selectedIndex).Rotate(transform.up, -userInput.Rotate * 2);
-        //explainText.
+    }
+    public IEnumerator AlphaText(int i)
+    {
+        Color c = explainText.GetChild(i).name.Contains("Text") ?
+            explainText.GetChild(i).GetComponent<Text>().color : explainText.GetChild(i).GetComponent<Image>().color;
+        bool isAlphaTrue = c.a == 1 ? true : false;
+        while (true)
+        {
+            while (!isAlphaTrue)
+            {
+                if (c.a >= 0.99f)
+                    isAlphaTrue = true;
+
+                c.a = Mathf.Lerp(c.a, 1, Time.deltaTime * textSpeed);
+                if (explainText.GetChild(i).name.Contains("Text"))
+                    explainText.GetChild(i).GetComponent<Text>().color = c;
+                else
+                    explainText.GetChild(i).GetComponent<Image>().color = c;
+                yield return null;
+            }
+            while (isAlphaTrue)
+            {
+                if (c.a <= 0.01f)
+                    isAlphaTrue = false;
+
+                c.a = Mathf.Lerp(c.a, 0, Time.deltaTime * textSpeed);
+                if (explainText.GetChild(i).name.Contains("Text"))
+                    explainText.GetChild(i).GetComponent<Text>().color = c;
+                else
+                    explainText.GetChild(i).GetComponent<Image>().color = c;
+                yield return null;
+            }
+            yield return null;
+        }
     }
     public void OnClickPreviewCharactor(int index)
     {
@@ -29,6 +75,7 @@ public class CharactorChoice : MonoBehaviour
         {
             if (i == index)
             {
+                transform.GetChild(i).transform.eulerAngles = new Vector3(0, 180, 0);
                 transform.GetChild(i).gameObject.SetActive(true);
                 content.GetChild(i).GetComponent<Image>().color = Color.gray;
                 selectedIndex = i;
