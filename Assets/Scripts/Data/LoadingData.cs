eb2f40f40405ce8e22fcc03ace692c9995cd3068
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-10-30 23:04:52 +0900

    update LoadingScene

--- Assets/Scripts/Data/LoadingData.cs
+++ Assets/Scripts/Data/LoadingData.cs
@@ -1,18 +1,28 @@
-using Newtonsoft.Json;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class LoadingData : MonoBehaviour
 {
     NativeLoadData nativeLoad = new NativeLoadData();
-    public GameObject startButton;
+    [SerializeField] private GameObject scrollbar_right;
+    [SerializeField] private GameObject scrollbar_left;
+    [SerializeField] private float scrollbarSpeed = 2;
+
+    private Scrollbar right;
+    private Scrollbar left;
+    private void Awake()
+    {
+        right = scrollbar_right.GetComponent<Scrollbar>();
+        left = scrollbar_left.GetComponent<Scrollbar>();
+    }
+
     private async void Start()
     {
+        scrollbar_left.SetActive(false);
+        StartCoroutine(StartLoading());
         //Native Data Load
         //nativeLoad.LoadNativeData();
 
@@ -54,11 +64,43 @@ public class LoadingData : MonoBehaviour
             //LandDataManager.Instance.LoadLandData();
             //LandDataManager.Instance.LoadBridge();
             //LandDataManager.Instance.isLoad = true;
-            startButton.SetActive(true);
+            //SceneManager.LoadScene(1);
         }
     }
-    public void OnClickStartButton()
+    public IEnumerator StartLoading()
     {
-        SceneManager.LoadScene(1);
+        float t = 0;
+        bool isRight = true;
+        while (true)
+        {
+            if (isRight)
+            {
+                t += Time.deltaTime * scrollbarSpeed;
+                if (t > 1)
+                {
+                    left.size = 1;
+                    scrollbar_right.SetActive(false);
+                    scrollbar_left.SetActive(true);
+                    t = 1;
+                    isRight = false;
+                }
+                right.size = t;
+            }
+            else
+            {
+
+                t -= Time.deltaTime * scrollbarSpeed;
+                if (t < 0)
+                {
+                    right.size = 0;
+                    scrollbar_right.SetActive(true);
+                    scrollbar_left.SetActive(false);
+                    t = 0;
+                    isRight = true;
+                }
+                left.size = t;
+            }
+            yield return null;
+        }
     }
 }
