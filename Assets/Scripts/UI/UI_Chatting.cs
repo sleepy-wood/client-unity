458aebc282645e49f9e033acd6e94c6a002595dc
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-05 22:32:14 +0900

    Update Chatting

--- Assets/Scripts/UI/UI_Chatting.cs
+++ Assets/Scripts/UI/UI_Chatting.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class UI_Chatting : MonoBehaviour
 {
     public Transform windowContent;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
     //private void Start()
     //{
     //    if(PhotonNetwork.PlayerList.Length <= 1)
@@ -14,6 +17,11 @@ public class UI_Chatting : MonoBehaviour
     //        transform.GetChild(0).gameObject.SetActive(false);
     //    }
     //}
+    private void Awake()
+    {
+        startPos = transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().position + new Vector3(-140, 0, 0);
+        endPos = transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().position;
+    }
     public void OnClickEmojiButton(int i)
     {
         //if (PhotonNetwork.PlayerList.Length <= 1) return;
@@ -25,10 +33,41 @@ public class UI_Chatting : MonoBehaviour
         Sprite emoji = Instantiate(emojiImgResource);
         emojiPrefab.GetComponent<Image>().sprite = emoji;
     }
-
-    public void OnClickChatActive()
+    bool isChatActive = false;
+    /// <summary>
+    /// Chatting Button을 눌렀을 때, 판넬을 활성화
+    /// </summary>
+    public void OnClickChat()
     {
-        //transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().position =
-        //왼쪽으로 140정도 이동
+        if (isChatActive)
+        {
+            isChatActive = false;
+            StopAllCoroutines();
+            //오른쪽으로 140정도 이동
+            StartCoroutine(ChatActive(endPos));
+        }
+        else
+        {
+            isChatActive = true;
+            StopAllCoroutines();
+            //왼쪽으로 140정도 이동
+            StartCoroutine(ChatActive(startPos));
+        }
     }
+    private IEnumerator ChatActive(Vector3 endPosition)
+    {
+        while (true)
+        {
+            transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().position = 
+                Vector3.Lerp(transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().position, endPosition, Time.deltaTime * 5f);
+            
+            if( Vector3.Distance(transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().position,endPosition) < 0.1f)
+            {
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
 }
