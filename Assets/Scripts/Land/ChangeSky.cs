b02b108b6e680a832482593020c6a55fa0998eed
suhyun <86099781+Suhyun77@users.noreply.github.com>
2022-11-08 04:11:02 +0900

    update Tree Flow

--- Assets/Scripts/Land/ChangeSky.cs
+++ Assets/Scripts/Land/ChangeSky.cs
@@ -6,6 +6,8 @@ public class ChangeSky : MonoBehaviour
 {
     public Material daySky;
     public Material nightSky;
+    public Material sunsetSky;
+
     public Light lt;
 
     public void Day()
@@ -18,5 +20,11 @@ public class ChangeSky : MonoBehaviour
     {
         RenderSettings.skybox = nightSky;
         lt.color = new Color(0.7058824f, 0.5764706f, 0.6941177f);
+    }
+
+    public void Sunset()
+    {
+        RenderSettings.skybox = sunsetSky;
+        lt.color = new Color(0.9245283f, 0.7477447f, 0.6140263f);
     }
 }
--- Assets/Scripts/Manager/TimeManager.cs
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -7,74 +7,157 @@ using System.Globalization;
 
 public class TimeManager : MonoBehaviour
 {
-    // 현재 시간
+    // 현재 시간, 날짜, 유저 데이터 
+    public Text txtCurrentTime, txtCurrentDate, txtUserData;
     public UDateTime now;
     // 나무 심은 누적 일
     public int totalPlantDay;
-    // 임시 현재 날짜 저장 변수
-    public UDateTime testFirstDay;
     // ChangeSky Script
     public ChangeSky sky;
-    // test 현재 날짜
-    public UDateTime testDate;
     // 나무를 처음 심은 날
     public UDateTime firstPlantDate;
+    public Text txtAge;
 
     private void Start()
     {
-        // Test : 현재 시간 저장
-        testFirstDay = DateTime.Now;
-        testDate = DateTime.Now;
+        firstPlantDate = DateTime.Now;
+        // 현재 날짜
+        now = DateTime.Now;
+        txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+
+        // 현재시간
+        txtCurrentTime.text = now.dateTime.ToString("tt h : mm");
+
+        // 현재 시간 고려해 SkyBox 세팅
+        // 낮 : 오전 7시~ / 일몰 : 오후 5시~ / 저녁 : 오후 6시~
+        if (DateTime.Now.Hour > 7 && DateTime.Now.Hour < 17) sky.Day();
+        else if (DateTime.Now.Hour > 17 && DateTime.Now.Hour < 18) sky.Sunset();
+        else
+        {
+            txtCurrentDate.color = Color.white;
+            txtCurrentTime.color = Color.white;
+            txtUserData.color = Color.white;
+            sky.Night();
+        }
+    }
+
+    bool isOnce;
+    bool skyChaneDone;
+    int i=0;
+    private void Update()
+    {
+        # region skyBox 변화 시연
+        // 특정 시간 지정
+        if (i == 0 && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            int month = int.Parse(DateTime.Now.ToString("MM"));
+            int day = int.Parse(DateTime.Now.ToString("dd"));
+            // 일몰 시간
+            now = new DateTime(2022, month, day, 17, 00, 0);
+            txtCurrentTime.text = now.dateTime.ToString("tt h : mm");
+            i++;
+        }
+        else if (i == 1 && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            int month = int.Parse(DateTime.Now.ToString("MM"));
+            int day = int.Parse(DateTime.Now.ToString("dd"));
+            // 저녁 시간
+            now = new DateTime(2022, month, day, 18, 00, 0);
+            txtCurrentTime.text = now.dateTime.ToString("tt h : mm");
+            i++;
+        }
+        else if (i == 2 && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            int month = int.Parse(DateTime.Now.ToString("MM"));
+            int day = int.Parse(DateTime.Now.ToString("dd"));
+            // 아침
+            now = new DateTime(2022, month, day + 1, 07, 00, 0);
+            CalculatePlantDays(now);
+            txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+            txtCurrentTime.text = now.dateTime.ToString("tt h : mm");
+            i++;
+            skyChaneDone = true;
+        }
+        #endregion
+        #region Day +1
+        if (totalPlantDay==0 && Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            CalculatePlantDays(now);
+            txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+        }
+        //else if (totalPlantDay==1 && Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    GameManager.Instance.treeController.dayCount++;
+        //    print(GameManager.Instance.treeController.dayCount);
+            
+        //    CalculatePlantDays(now);
+        //    txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+        //}
+        else if(skyChaneDone && totalPlantDay==2 && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            now = now.dateTime.AddDays(1);
+            CalculatePlantDays(now);
+            txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+        }
+        else if(totalPlantDay==3 && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            now = now.dateTime.AddDays(1);
+            CalculatePlantDays(now);
+            txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+        }
+        else if(totalPlantDay==4 && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            now = now.dateTime.AddDays(1);
+            CalculatePlantDays(now);
+            txtCurrentDate.text = now.dateTime.ToString("yyyy-MM-dd") + " (" + totalPlantDay + "일차)";
+        }
+        #endregion
+
+
+        // SkyBox 변경
+        if (txtCurrentTime.text == "오후 5 : 00" && !isOnce)
+        {
+            sky.Sunset();
+            txtCurrentDate.color = Color.black;
+            txtCurrentTime.color = Color.black;
+            txtUserData.color = Color.black;
+            isOnce = true;
+        }
+        else if (txtCurrentTime.text == "오후 6 : 00" && isOnce)
+        {
+            sky.Night();
+            txtCurrentDate.color = Color.white;
+            txtCurrentTime.color = Color.white;
+            txtUserData.color = Color.white;
+            isOnce = false;
+        }
+        else if (txtCurrentTime.text == "오전 7 : 00" && !isOnce)
+        {
+            sky.Day();
+            txtCurrentDate.color = Color.black;
+            txtCurrentTime.color = Color.black;
+            txtUserData.color = Color.black;
+            isOnce = true;
+        }
     }
 
     /// <summary>
-    /// 사용자가 MyRoom에 들어왔을 때 나무 심은 누적 Day Count해주는 함수
+    /// 내가 입력한 Date - 나무 처음 심은 Date
     /// </summary>
     /// <returns>나무 심은지 몇일이 지났는지 int로 반환</returns>
     public void CalculatePlantDays(DateTime date)
     {
-        // Test 용
-        TimeSpan timeDif = date - testFirstDay;
-        // 실제 구현용
-        //TimeSpan timeDif = DateTime.Now - start;
-        totalPlantDay = (int)timeDif.Days;
-        //return totalPlantDay;
-    }
+        TimeSpan timeDif = date - firstPlantDate;
+        totalPlantDay = (int)timeDif.Days + 1;
+        GameManager.Instance.treeController.dayCount = totalPlantDay;
 
-    public Text txtDayCount;
-    public Text txtAge;
-    /// <summary>
-    /// Test : 버튼 누르면 Plus Day
-    /// </summary>
-    public void onPlusDay()
-    {
-        testDate = testDate.dateTime.AddHours(25);
-        CalculatePlantDays(testDate);
-        // Tree Update
-        GameManager.Instance.treeController.TreeUpdate();
-    }
-
-
-    bool isDay = true;
-    /// <summary>
-    /// 6시 기준으로 오전 오후 Skybox 변경
-    /// </summary>
-    public void onChangeSky()
-    {
-        // 실제 구현용
-        //if (now.dateTime.ToString("tt") == "오전")
-        // Test
-        if (!isDay)
+        if (totalPlantDay == 1)
         {
-            sky.Day();
-            isDay = true;
+            GameManager.Instance.treeController.LoadTree();
         }
-        else
+        else if (totalPlantDay == 2)
         {
-            sky.Night();
-            isDay = false;
+            GameManager.Instance.treeController.LoadTree();
         }
-
     }
-
 }
