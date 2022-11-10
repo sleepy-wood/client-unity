8f7cbe72867c474ea2d5f55bf55fe90a2e6b2e40
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-10 19:03:05 +0900

    .

---
+++ Assets/Scripts/UI/Resolution.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resolution : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasScaler cs = GetComponent<CanvasScaler>();
+
+        float ratioX = Screen.width / 600.0f;// 600.0f;
+        float ratioY = Screen.height / 800.0f;
+
+        if (ratioX < ratioY) cs.scaleFactor = ratioX;
+        else cs.scaleFactor = ratioY;
+    }
+
+}
