374d6f72e7c8fadb73262189fe5e505f5500fc98
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-17 22:26:02 +0900

    update TreeGraph

---
+++ Assets/Scripts/UI/Graph/TreeGraph_UI.cs
@@ -0,0 +1,137 @@
+using Cinemachine;
+using NativePlugin.HealthData;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeGraph_UI : MonoBehaviour
+{
+    [SerializeField] private Transform m_scrollBarParent;
+    GetTreeData treeData;
+    SleepSample[] samples;
+    List<float> treeSleepTimes = new List<float>();
+    bool isOnce = false;
+    private void Awake()
+    {
+       for(int i = 0; i <  DataTemporary.GetTreeData.getTreeDataList.Count; i++)
+       {
+            if(DataTemporary.MyUserData.currentLandId == DataTemporary.GetTreeData.getTreeDataList[i].landId)
+            {
+                treeData = DataTemporary.GetTreeData.getTreeDataList[i];
+                break;
+            }
+       }
+       samples = DataTemporary.samples;
+    }
+    private void OnEnable()
+    {
+        DrawTreeGraph();
+    }
+    private void DrawTreeGraph()
+    {
+        DateTime treeDateTime = DateTime.Parse(treeData.createdAt);
+        //시작점을 구하자
+        int i = 0;
+        bool isStartDay = false;
+        for (i = 0; i < samples.Length; i++)
+        {
+            if (samples[i].Type.ToString().Contains("Asleep"))
+            {
+                //두 시간의 중앙값을 알아내어 어느 날에 속하게 할 것인지 정하기
+                TimeSpan diff = samples[i].EndDate - samples[i].StartDate;
+                diff /= 2;
+                var NewDate = new DateTime(
+                    samples[i].StartDate.Year,
+                    samples[i].StartDate.Month,
+                    samples[i].StartDate.Day,
+                    samples[i].StartDate.Hour,
+                    samples[i].StartDate.Minute,
+                    samples[i].StartDate.Second
+                    );
+                NewDate.AddDays(diff.Days);
+                NewDate.AddHours(diff.Hours);
+                NewDate.AddMinutes(diff.Minutes);
+                NewDate.AddSeconds(diff.Seconds);
+                if(NewDate == treeDateTime && !isStartDay)
+                {
+                    isStartDay = true;
+                }
+                else
+                {
+                    if (isStartDay)
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+        }
+        //시작으로부터 5일까지 데이터 가져오기
+        int startDay = 0;
+        int preDay = 0;
+        int cnt = 0;
+        float times = 0;
+        for (int j = i; j >= 0; j--)
+        {
+            if (samples[j].Type.ToString().Contains("Asleep"))
+            {
+                //두 시간의 중앙값을 알아내어 어느 날에 속하게 할 것인지 정하기
+                TimeSpan diff = samples[j].EndDate - samples[j].StartDate;
+                diff /= 2;
+                var NewDate = new DateTime(
+                    samples[j].StartDate.Year,
+                    samples[j].StartDate.Month,
+                    samples[j].StartDate.Day,
+                    samples[j].StartDate.Hour,
+                    samples[j].StartDate.Minute,
+                    samples[j].StartDate.Second
+                    );
+                NewDate.AddDays(diff.Days);
+                NewDate.AddHours(diff.Hours);
+                NewDate.AddMinutes(diff.Minutes);
+                NewDate.AddSeconds(diff.Seconds);
+                //중앙값의 날의 요일
+                startDay = (int)NewDate.DayOfWeek;
+
+                if (!isOnce)
+                {
+                    isOnce = true;
+                    preDay = startDay;
+                }
+                if (cnt >= 5)
+                    break;
+                if (preDay != startDay)
+                {
+                    treeSleepTimes.Add(times);
+                    times = 0;
+                    cnt++;
+                }
+                else
+                {
+                    times += (float)diff.TotalSeconds;
+                }
+                preDay = startDay;
+            }
+        }
+        for(int j = 0;j < treeSleepTimes.Count; j++)
+        {
+            StartCoroutine(GraphMove(m_scrollBarParent.GetChild(j).GetComponent<Scrollbar>(), treeSleepTimes[j] / 86400));
+        }
+    }
+
+    #region Coroutine
+    private IEnumerator GraphMove(Scrollbar scrollbar, float size)
+    {
+        float t = 0;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * 0.05f;
+            scrollbar.size = Mathf.Lerp(scrollbar.size, size, t);
+            yield return null;
+        }
+    }
+    #endregion
+}
