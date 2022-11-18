5cc5953332ee7a2fdaba24fd9120891d71e9b8e2
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-18 18:41:55 +0900

    update Data

--- Assets/Scripts/Data/DataModule.cs
+++ Assets/Scripts/Data/DataModule.cs
@@ -3,6 +3,7 @@ using Cysharp.Threading.Tasks;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
