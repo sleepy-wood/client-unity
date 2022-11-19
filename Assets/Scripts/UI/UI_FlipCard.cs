4d87c1a598427a5378fb6380889fc5ab7a0ae025
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-20 04:13:43 +0900

    update Collection

---
+++ Assets/Scripts/UI/UI_FlipCard.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_FlipCard : MonoBehaviour
+{
+    private GameObject cardBack;
+    private GameObject cardFront;
+    private bool cardBackIsActive;
+    private int timer;
+    private void Awake()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Button>().onClick.AddListener(
+                () => OnButtonClickFlipCard());
+        }
+    }
+    private void Start()
+    {
+        cardBack = transform.GetChild(1).gameObject;
+        cardFront = transform.GetChild(0).gameObject;
+    }
+    public void StartFlip()
+    {
+        StopAllCoroutines();
+        StartCoroutine(CalcFlip());
+    }
+
+
+    public void OnButtonClickFlipCard()
+    {
+        StartFlip();
+    }
+    public void Flip()
+    {
+        if (cardBackIsActive)
+        {
+            cardBackIsActive = false;
+            cardBack.SetActive(false);
+            cardFront.SetActive(true);
+        }
+        else
+        {
+            cardBackIsActive = true;
+            cardBack.SetActive(true);
+            cardFront.SetActive(false);
+        }
+    }
+    public IEnumerator CalcFlip()
+    {
+        for(int i = 0; i < 180; i++)
+        {
+            yield return new WaitForSeconds(0.001f);
+            transform.Rotate(new Vector3(0,1,0));
+            timer++;
+
+            if (timer == 90 || timer == -90)
+                Flip();
+        }
+        timer = 0;
+    }
+}
