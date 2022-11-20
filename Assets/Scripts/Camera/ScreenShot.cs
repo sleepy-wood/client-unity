95bd80a5b2194e65915f4547047cb7da5c35a134
suhyun <86099781+Suhyun77@users.noreply.github.com>
2022-11-20 19:23:21 +0900

    update Video Capture

--- Assets/Scripts/Camera/ScreenShot.cs
+++ Assets/Scripts/Camera/ScreenShot.cs
@@ -1,45 +1,142 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
+using System;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ScreenShot : MonoBehaviour
 {
-        // 스크린샷 하는 카메라
-        public Camera screenshotCam;
+    public Transform previewTreePos;
+    public GameObject previewTree;
+    public Image treeCaptureImg;
 
-        private int resWidth;
-        private int resHeight;
-        string path;
+    /// <summary>
+    /// 사용자가 프로필 버튼 누르면 그 즉시 스크린샷해서 트리 리스트 이미지 업데이트
+    /// </summary>
 
-        // Use this for initialization
-        void Start()
+    #region ScreenShot
+    //    public void ScreenShotUpdate()
+    //    {
+    //        // 타겟 렌더 텍스쳐
+    //        RenderTexture renderTexture = GetComponent<Camera>().targetTexture;
+    //        // 화면 크기의 텍스쳐 생성
+    //        Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
+    //        RenderTexture.active = renderTexture;
+    //        // 캡쳐 영역 지정 뒤, 현재 화면 픽셀들을 텍스쳐 픽셀에 저장
+    //        texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+    //        texture.Apply();
+
+    //        // Save Image File
+    //#if UNITY_STANDALONE
+    //        string path = $"{Application.dataPath}/Resources/ScreenShot/TreeCapture.png";
+    //#elif UNITY_IOS || UNITY_ANDROID
+    //        string path = $"{Application.persistentDataPath}/TreeCapture.png";
+    //#endif
+    //        // 텍스쳐를 PNG 포맷의 byte[]로 변환한 뒤 파일 경로에 작성
+    //        File.WriteAllBytes(path, texture.EncodeToPNG());
+    //        byte[] data = File.ReadAllBytes(path);
+
+    //        // Create the texture
+    //        Texture2D screenshotTexture = new Texture2D(renderTexture.width, renderTexture.height);
+
+    //        // Load the image
+    //        screenshotTexture.LoadImage(data);
+
+    //        // Create a sprite
+    //        Sprite screenshotSprite = Sprite.Create(screenshotTexture, new Rect(0, 0, renderTexture.width, renderTexture.height), new Vector2(0.5f, 0.5f));
+
+    //        // Set the sprite to the screenshotPreview
+    //        treeCaptureImg.GetComponent<Image>().sprite = screenshotSprite;
+    //        // Update TreeList Image 
+    //        //Sprite s = Sprite.Create(texture, new Rect(0, 0, renderTexture.width, renderTexture.height), new Vector2(0.5f, 0.5f));
+    //        //treeCaptureImg.sprite = s;
+    //    }
+    #endregion
+
+    bool once = false;
+    private void Update()
+    {
+        if (previewTree == null)
         {
-                resWidth = Screen.width;
-                resHeight = Screen.height;
-                path = Application.dataPath + "/ScreenShot/";
-                Debug.Log(path);
+            previewTree = GameObject.Find("previewTree");
+            previewTree.layer = 11;  // Tree Layer
         }
+    }
 
-        public void ClickScreenShot()
+
+    bool succeded = true;
+    string path;
+
+    #region OnPosterRender
+    // 카메라가 렌더링하는 부분만 캡쳐
+    /// <summary>
+    /// 카메라와 함께있는 컴포넌트에 작성했을 때 동작
+    /// 프레임마다 해당 카메라의 렌더링이 끝냔 후에 호출됨 (카메라가 렌더링을 마친 모습만 저장)
+    /// </summary>
+    //private void OnPostRender()
+    #endregion
+
+    public void SaveCameraView()
+    {
+        // Screen Capture Code
+
+        RenderTexture screenTexture = new RenderTexture(Screen.width, Screen.height, 16);
+        Camera cam = GetComponent<Camera>();
+        cam.targetTexture = screenTexture;
+        RenderTexture.active = screenTexture;
+        cam.Render();
+
+        // 화면 크기의 텍스쳐 생성
+        Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
+        // 캡쳐 영역 지정 뒤
+        Rect area = new Rect(0, 0, Screen.width, Screen.height);
+        // 현재 화면 픽셀들을 텍스쳐 픽셀에 저장
+        renderedTexture.ReadPixels(area, 0, 0);
+        RenderTexture.active = null;
+        byte[] byteArray = renderedTexture.EncodeToPNG();
+
+        try
         {
-                DirectoryInfo dir = new DirectoryInfo(path);
-                if (!dir.Exists)
+#if UNITY_STANDALONE
+            path = $"{Application.dataPath}/ScreenShot/TreeCapture_{GameManager.Instance.treeController.treeId}.png";
+            if (Directory.Exists($"{Application.dataPath}/ScreenShot") == false)
+            {
+                Directory.CreateDirectory($"{Application.dataPath}/ScreenShot");
+            }
+            else
+            {
+                File.Delete(path);
+            }
+#elif UNITY_IOS || UNITY_ANDROID
+                path = $"{Application.persistentDataPath}/ScreenShot/TreeCapture_{GameManager.Instance.treeController.treeId}.png";
+                if (Directory.Exists($"{Application.persistentDataPath}/ScreenShot") == false)
                 {
-                        Directory.CreateDirectory(path);
+                    Directory.CreateDirectory($"{Application.persistentDataPath}/ScreenShot");
                 }
-                string name;
-                name = path + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
-                RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-                screenshotCam.targetTexture = rt;
-                Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-                Rect rec = new Rect(0, 0, screenShot.width, screenShot.height);
-                screenshotCam.Render();
-                RenderTexture.active = rt;
-                screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-                screenShot.Apply();
-
-                byte[] bytes = screenShot.EncodeToPNG();
-                File.WriteAllBytes(name, bytes);
+                else
+                {
+                    File.Delete(path);
+                }
+#endif
+            // 스크린샷 저장
+            File.WriteAllBytes(path, byteArray);
         }
+        catch (Exception e)
+        {
+            succeded = false;
+            Debug.LogWarning($"Screenshot Save Failed :  {path}");
+            Debug.LogWarning(e);
+        }
+
+        if (succeded)
+        {
+            Debug.Log($"Screenshot Save Succeded :  {path}");
+        }
+    }
 }
