23df25189fdc8f9581531d3516bb2d1107a8cbd6
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-20 10:10:04 +0900

    Update LoadingData.cs

--- Assets/Scripts/Data/LoadingData.cs
+++ Assets/Scripts/Data/LoadingData.cs
@@ -216,47 +216,47 @@ public class LoadingData : MonoBehaviourPunCallbacks
             else
             {
 
-                if (marketsData[h].result)
-                {
-#if UNITY_STANDALONE
-                    string path = Application.dataPath + "/MarketImg/" + (Category)h;
-#elif UNITY_IOS || UNITY_ANDROID
-            string path = Application.persistentDataPath + "/MarketImg/" + (Category)h;
-#endif
-                    if (!Directory.Exists(path))
-                    {
-                        Directory.CreateDirectory(path);
-                    }
+//                if (marketsData[h].result)
+//                {
+//#if UNITY_STANDALONE
+//                    string path = Application.dataPath + "/MarketImg/" + (Category)h;
+//#elif UNITY_IOS || UNITY_ANDROID
+//            string path = Application.persistentDataPath + "/MarketImg/" + (Category)h;
+//#endif
+//                    if (!Directory.Exists(path))
+//                    {
+//                        Directory.CreateDirectory(path);
+//                    }
 
-                    if (marketsData[h].data.Count == 0)
-                    {
-                        continue;
-                    }
+//                    if (marketsData[h].data.Count == 0)
+//                    {
+//                        continue;
+//                    }
 
-                    for (int i = 0; i < marketsData[h].data.Count; i++)
-                    {
-                        for (int j = 0; j < marketsData[h].data[i].orderDetails.Count; j++)
-                        {
-                            List<ProductImages> productImages = new List<ProductImages>();
-                            productImages = marketsData[h].data[i].orderDetails[j].product.productImages;
-                            for (int k = 0; k < productImages.Count - 1; k++)
-                            {
-                                if (productImages[k].mimeType.Split('/')[0] == "image")
-                                {
-                                    Texture2D texture = await DataModule.WebrequestTexture(productImages[k].path, DataModule.NetworkType.GET);
-                                    byte[] bytes = texture.EncodeToPNG();
-                                    File.WriteAllBytes(path + "/Market_" + productImages[k].originalName + ".png", bytes);
-                                }
-                                else
-                                {
-                                    if (productImages[k].path.Split('.').Length > 0)
-                                        continue;
-                                    AssetBundle assetBundle = await DataModule.WebRequestAssetBundle(productImages[k].path, DataModule.NetworkType.GET, DataModule.DataType.ASSETBUNDLE, (Category)h + "/" + productImages[k].originalName);
-                                }
-                            }
-                        }
-                    }
-                }
+//                    for (int i = 0; i < marketsData[h].data.Count; i++)
+//                    {
+//                        for (int j = 0; j < marketsData[h].data[i].orderDetails.Count; j++)
+//                        {
+//                            List<ProductImages> productImages = new List<ProductImages>();
+//                            productImages = marketsData[h].data[i].orderDetails[j].product.productImages;
+//                            for (int k = 0; k < productImages.Count - 1; k++)
+//                            {
+//                                if (productImages[k].mimeType.Split('/')[0] == "image")
+//                                {
+//                                    Texture2D texture = await DataModule.WebrequestTexture(productImages[k].path, DataModule.NetworkType.GET);
+//                                    byte[] bytes = texture.EncodeToPNG();
+//                                    File.WriteAllBytes(path + "/Market_" + productImages[k].originalName + ".png", bytes);
+//                                }
+//                                else
+//                                {
+//                                    if (productImages[k].path.Split('.').Length > 0)
+//                                        continue;
+//                                    AssetBundle assetBundle = await DataModule.WebRequestAssetBundle(productImages[k].path, DataModule.NetworkType.GET, DataModule.DataType.ASSETBUNDLE, (Category)h + "/" + productImages[k].originalName);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
             }
         }
 
