428bb4c757ad46072f045416e6b3f104deeeb9b6
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-26 15:20:27 +0900

    Update SharedLand

--- Assets/Scripts/Other/ChoicePosition.cs
+++ Assets/Scripts/Other/ChoicePosition.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class ChoicePosition : MonoBehaviour, IClickedObject
 {
     public GameObject collection;
+    private bool isCreate = false;
     public void ClickMe()
     {
-        collection.SetActive(true);
-        collection.GetComponent<Choice>().SettingChoicePos(transform.position);
+        Debug.Log("클릭됨: " + gameObject);
+        if (!isCreate)
+        {
+            collection.SetActive(true);
+            collection.GetComponent<Choice>().SettingChoicePos(int.Parse(gameObject.name[gameObject.name.Length - 1].ToString()));
+            isCreate = true;
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+        }
     }
 
     public void StairMe()
--- Assets/Scripts/Other/ChoiceRespawnPos.cs
+++ Assets/Scripts/Other/ChoiceRespawnPos.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class ChoiceRespawnPos : MonoBehaviour
 {
-    private Camera camera;
+    private Camera camera_share;
     public GameObject collection;
 
     private void Start()
     {
-        camera = transform.GetChild(0).GetComponent<Camera>(); 
+        camera_share = transform.GetChild(0).GetComponent<Camera>(); 
     }
 
     private void Update()
     {
         Vector3 mousePos = Input.mousePosition;
-        Ray ray = camera.ScreenPointToRay(mousePos);
+        Ray ray = camera_share.ScreenPointToRay(mousePos);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            if (hit.transform.GetComponent<IClickedObject>() != null)
+            if (hit.transform.GetComponent<IClickedObject>() != null && Input.GetMouseButtonDown(0))
             {
                 hit.transform.GetComponent<IClickedObject>().ClickMe();
             }
@@ -28,5 +28,10 @@ public class ChoiceRespawnPos : MonoBehaviour
     public void OnClickBackButton()
     {
         collection.SetActive(false);
+        camera_share.gameObject.SetActive(false);
+    }
+    public void OnClickCreateButton()
+    {
+        camera_share.gameObject.SetActive(true);
     }
 }
