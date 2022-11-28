88fe3d6e58250bdd2f3388fc80db9f2c2bb85ecd
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-11-28 20:05:40 +0900

    Update SharedLand

--- Assets/Scripts/Other/ChoicePosition.cs
+++ Assets/Scripts/Other/ChoicePosition.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ChoicePosition : MonoBehaviour, IClickedObject
+public class ChoicePosition : MonoBehaviour
 {
     public GameObject collection;
     private bool isCreate = false;
-    public void ClickMe()
+    //public void ClickMe()
+    //{
+    //    Debug.Log("클릭됨: " + gameObject);
+    //    if (!isCreate)
+    //    {
+    //        collection.transform.parent.GetComponent<Choice>().SettingChoicePos(int.Parse(gameObject.name[gameObject.name.Length - 1].ToString()));
+    //        collection.SetActive(true);
+    //        isCreate = true;
+    //    }
+    //}
+
+    //public void StairMe()
+    //{
+    //}
+
+    public void OnClickHere()
     {
-        Debug.Log("클릭됨: " + gameObject);
-        if (!isCreate)
+        collection.transform.parent.GetComponent<Choice>().SettingChoicePos(int.Parse(gameObject.name[gameObject.name.Length - 1].ToString()));
+        collection.SetActive(true);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("User"))
         {
-            collection.transform.parent.GetComponent<Choice>().SettingChoicePos(int.Parse(gameObject.name[gameObject.name.Length - 1].ToString()));
-            collection.SetActive(true);
-            isCreate = true;
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("User"))
+        {
+            transform.GetChild(2).gameObject.SetActive(false);
         }
     }
 
-    public void StairMe()
-    {
-    }
     private void Update()
     {
         if (ChoiceRespawnPos.Instance.isCompleteList[int.Parse(gameObject.name[gameObject.name.Length - 1].ToString())])
