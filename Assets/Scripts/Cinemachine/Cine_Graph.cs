f387a706368340ef88f1f49ba419a9f2c45bfb45
DAERYEOL <47146732+DAERYEOL@users.noreply.github.com>
2022-11-29 00:00:28 +0900

    Update Loding Scene

--- Assets/Scripts/Cinemachine/Cine_Graph.cs
+++ Assets/Scripts/Cinemachine/Cine_Graph.cs
@@ -48,20 +48,25 @@ public class Cine_Graph : MonoBehaviour
     IEnumerator GraphMove(Vector3 value_scale, Vector3 value, int idx)
     {
         float a, b, c;
-        a = Random.Range(1, 5) * 0.4f;
+        a = Random.Range(1, 8) * 0.1f;
         b = Random.Range(1, 5) * 0.4f;
         c = Random.Range(1, 5) * 0.4f;
-        Color endColor = new Color(a, b, c, 0.7f);
+        
+        Color endColor = new Color(a, 0.1f, 1.0f);
+      //  Color startColor = new Color(0.54f - a, 0.4f, 0.55f, 0.7f);
         float t = 0;
         while (t < 1f)
         {
             t += Time.deltaTime * 0.1f;
             transform.GetChild(idx).localPosition = Vector3.Lerp(transform.GetChild(idx).localPosition, value, t);
             transform.GetChild(idx).localScale = Vector3.Lerp(transform.GetChild(idx).localScale, value_scale, t);
-            transform.GetChild(idx).GetComponent<MeshRenderer>().material.color =
-                Color.Lerp(transform.GetChild(idx).GetComponent<MeshRenderer>().material.color, endColor, t);
+            transform.GetChild(idx).GetComponent<MeshRenderer>().material.color = new Color(0.7f, 0.5f, 1.0f, 1.0f);
+            transform.GetChild(idx).GetComponent<MeshRenderer>().material.SetColor("_EmissionColor",endColor);
+           // Color.Lerp(, endColor, t);
+            //Color.Lerp(transform.GetChild(idx).GetComponent<MeshRenderer>().material.GetColor("_EmissionColor"), endColor, t);
             yield return null;
         }
+        //transform.GetChild(idx).GetComponent<MeshRenderer>().material.color = new Color(0.7f, 0.5f, 1.0f, 0.0f);
         transform.GetChild(idx).GetComponent<MeshRenderer>().material.color = endColor;
         transform.GetChild(idx).localPosition = value;
         transform.GetChild(idx).localScale = value_scale;
